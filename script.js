a9816bbc754c9f0584f00af147bc8261a352f9ff
Dunay Gudratli <dqudretli@std.beu.edu.az>
2026-02-03 23:28:45 +0400

    254: store data in local storage

--- script.js
+++ script.js
@@ -69,11 +69,11 @@ class App {
 
   constructor() {
     this._getPosition();
+    this._getLocalStorage();
 
+    // Event handlers
     form.addEventListener('submit', this._newWorkout.bind(this));
-
     inputType.addEventListener('change', this._toggleElevationField);
-
     containerWorkouts.addEventListener('click', this._moveToPopup.bind(this));
   }
 
@@ -100,6 +100,10 @@ class App {
     }).addTo(this.#map);
 
     this.#map.on('click', this._showForm.bind(this));
+
+    this.#workouts.forEach(work => {
+      this._renderWorkoutMarker(work);
+    });
   }
 
   _showForm(mapE) {
@@ -171,6 +175,8 @@ class App {
     this._renderWorkout(workout);
 
     this._hideForm();
+
+    this._setLocalStorage();
   }
 
   _renderWorkoutMarker(workout) {
@@ -254,9 +260,28 @@ class App {
       },
     });
 
-    workoutObj.click();
+    // workoutObj.click();
+  }
 
-    console.log(workoutObj);
+  _setLocalStorage() {
+    localStorage.setItem('workouts', JSON.stringify(this.#workouts));
+  }
+
+  _getLocalStorage() {
+    const data = JSON.parse(localStorage.getItem('workouts'));
+
+    if (!data) return;
+
+    this.#workouts = data;
+
+    this.#workouts.forEach(work => {
+      this._renderWorkout(work);
+    });
+  }
+
+  reset() {
+    localStorage.removeItem('workouts');
+    location.reload();
   }
 }
 
